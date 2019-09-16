25507e08fe8c59af7258c6003e98ad7e4004b0c5
dominguez-tony <tony.dominguez84@gmail.com>
2019-09-16 23:25:03 +0200

    affichage admin index et edit

---
+++ src/Controller/Admin/AdminPropertyController.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Repository\PropertyRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Property;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+
+
+
+class AdminPropertyController extends AbstractController{
+
+
+ /**
+    * @route("/admin", name="admin")
+    *@param PropertyRepository  $repository
+    * @return Response
+     */
+    
+
+    public function index(PropertyRepository $repository): Response
+    {
+    
+    
+        $properties = $repository->findAll();
+    
+    return $this->render('admin/property/index.html.twig', [
+        'properties' => $properties
+    ]);
+    }
+
+
+ /**
+    * @route("/admin/property/{id}", name="admin.property.edit")
+    * @return Response
+     */
+    
+
+    public function edit(): Response
+    {
+    
+    
+       
+    
+    return $this->render('admin/property/edit.html.twig');
+    }
+
+
+    
+    }
+
